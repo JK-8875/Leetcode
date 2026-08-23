662247636884cf14a6197eac75fbab0d4afd7418
Jai <jaik19278@gmail.com>
2026-08-23 20:22:42 +0530

    Time: 8 ms (53.28%) | Memory: 46.9 MB (83.94%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int leftSum = 0, rightSum= 0;
+        int leftQ = 0, rightQ = 0;
+
+        for(int i = 0; i<n/2; i++){
+            char ch = num.charAt(i);
+            if(ch == '?'){
+                leftQ++;
+            } else{
+                leftSum += ch-'0';
+            }
+        }
+        for(int i = n/2; i<n; i++){
+            char ch = num.charAt(i);
+            if (ch == '?'){
+                rightQ++;
+            } else{
+                rightSum += ch-'0';
+            
+            }
+        }
+        if ((leftQ + rightQ)%2 ==1){
+            return true;
+        }
+        return 2*(leftSum-rightSum)!=9*(rightQ- leftQ);
+        
+    }
+}
